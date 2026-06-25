1dddda4292efe54cca1309fcfe6f9452a7ac4793
tdelmas2 <tdelmas2@f4r11s8.paris.42.school>
2026-06-25 18:13:15 +0200

    Initial commit

---
+++ ex00/includes/BitcoinExchange.hpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdelmas2 <tdelmas2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/17 11:56:27 by tdelmas2          #+#    #+#             */
+/*   Updated: 2026/06/25 18:11:17 by tdelmas2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINTEXCHANGE_HPP
+#define BITCOINTEXCHANGE_HPP
+
+#include <fstream>
+#include <iostream>
+#include <exception>
+#include <string>
+#include <cctype>
+#include <map>
+#include <sstream>
+
+class BitcoinExchange{
+    private:
+    void CheckFormat(std::string &Line, bool IsFirstLine, bool isCsv);
+    std::string trim (const std::string &str);
+    void CheckDate(std::string const &date) const;
+    void CheckValue(std::string const &value) const;
+    bool LoadDatabase(const std::string &file, std::map<std::string, float> &database);
+    
+    public:
+    BitcoinExchange();
+    BitcoinExchange(BitcoinExchange const &src);
+    BitcoinExchange &operator=(BitcoinExchange const &rhs);
+    ~BitcoinExchange();
+
+    void Conversion(std::string input) const;
+
+    class DateValue : public std::exception
+    {
+        public:
+        virtual const char *what() const throw()
+        {
+            return "Error: The indication 'date | value' must be the first line of the document.";
+        }
+    };
+    class Negative : public std::exception
+    {
+        public:
+        virtual const char *what() const throw()
+        {
+            return "Error: Not a positive number.";
+        }
+    };
+    class GoodExample : public std::exception
+    {
+        public:
+        virtual const char *what() const throw()
+        {
+            return "Error: The good format is : 'YYYY-MM-DD | value.";
+        }
+    };
+    class ValueFormat : public std::exception
+    {
+        public:
+        virtual const char *what() const throw()
+        {
+            return "Error: The value format isn't correct";
+        }
+    };
+};
+
+
+#endif
---
+++ ex00/src/BitcoinExchange.cpp
@@ -0,0 +1,141 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdelmas2 <tdelmas2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/24 21:39:07 by tdelmas2          #+#    #+#             */
+/*   Updated: 2026/06/25 18:11:25 by tdelmas2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/BitcoinExchange.hpp"
+
+std::string trim (const std::string &str){
+    size_t first = 0;
+    size_t last = str.size();
+
+    while (first < last && std::isspace(static_cast <unsigned char>(str[first])))
+        first++;
+    while (last > first && std::isspace(static_cast <unsigned char>(str[last])))
+        last--;
+    return str.substr(first, last - first);
+}
+
+void BitcoinExchange::CheckDate(std::string const &date) const{
+    if (date.size() != 10 || date[4] != '-' || date[7] != '-')
+        throw GoodExample();
+    for (size_t i = 0; i < date.size(); i++) {
+        if (i == 4 || i == 7) 
+            continue;
+        if (!std::isdigit(static_cast<unsigned char>(date[i])))
+            throw GoodExample();
+    }
+}
+
+void BitcoinExchange::CheckValue(std::string const &value) const{
+    if (value.empty())
+        throw GoodExample();
+
+    size_t i = 0;
+
+    if (value[i] == '+')
+        i++;
+    if (value[i] == '-')
+        throw Negative();
+
+    size_t pointCount = 0;
+    size_t digitCount = 0;
+
+    for (; i < value.size(); i++)
+    {
+        if (value[i] == '.'){
+            pointCount++;
+            if (pointCount > 1 || digitCount == 0 || i == value.size() - 1)
+                throw ValueFormat();
+        }
+        else if (std::isdigit(static_cast<unsigned char>(value[i]))) {
+            digitCount++;
+        } else {
+            throw ValueFormat();
+        }
+    }
+    if (digitCount == 0)
+        throw ValueFormat();
+}
+
+
+void BitcoinExchange::CheckFormat(std::string &line, bool isFirstLine, bool isCsv){
+    size_t pipePos;
+
+    if (line == "date | value" && isFirstLine == false)
+        throw DateValue();
+    if (!isCsv)
+        pipePos = line.find('|');
+    else if (isCsv)
+        pipePos = line.find(',');
+    if (pipePos == std::string::npos)
+        throw GoodExample();
+    std::string date = trim(line.substr(0, pipePos));
+    std::string valuePart = trim (line.substr(pipePos + 1));
+    
+    CheckDate(date);
+    CheckValue(valuePart);
+}
+
+bool BitcoinExchange::LoadDatabase(const std::string &filename, std::map<std::string, float> &database){
+    std::ifstream file(filename.c_str());
+    if (!file.is_open())
+    {
+        std::cout << "Error: could not open the file." << "\n";
+        return false;
+    }
+
+    std::string line;
+    bool isFirstLine = true;
+
+    while (std::getline(file, line))
+    {
+        if (line.empty())
+            continue;
+        try
+        {
+            CheckFormat(line, isFirstLine, true);
+            if (isFirstLine) {
+                isFirstLine = false;
+                continue;
+            }
+            size_t commaPos = line.find(',');
+            std::string datePart = trim(line.substr(0, commaPos));
+            std::string pricePart = trim(line.substr(commaPos + 1));
+            float price;
+            std::stringstream ss(pricePart);
+            ss >> price;
+            if (!(ss >> price))
+                throw ValueFormat();
+            database[datePart] = price;
+        }
+        catch (const std::exception &e)
+        { std::cout << e.what() << "\n"; }
+    }
+    file.close();
+    return true;
+}
+    
+BitcoinExchange::BitcoinExchange(){
+    
+}
+    
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &src){
+    
+}
+    
+BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange const &rhs){
+    
+}
+    
+BitcoinExchange::~BitcoinExchange(){
+    
+}
+    
